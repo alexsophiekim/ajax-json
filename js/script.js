215dfe0991c26e5b02e4c9ce1260f4e96e9b1fcf
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-07-30 22:04:47 +0000

    create mock_data

--- js/script.js
+++ js/script.js
@@ -6,11 +6,26 @@ $(document).ready(function(){
     // console.log('button has been clicked');
     $.ajax({
       type: 'GET',
-      url: 'people.json',
+      url: 'data/MOCK_DATA.json',
       dataType: 'json',
       success: function(data){
         console.log(data);
         console.log('success');
+        data.map(function(person){
+          let card = `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">`;
+          card += `<div class="card h-100" style="width: 15rem;">`;
+          card += `<img src="${person['avatar']}" class="card-img-top" alt="...">`;
+          card += `<div class="card-body">`;
+          card += `<h5 class="card-text">${person['first_name']} ${person['last_name']}</h5>`;
+          card += `<p class="card-text">${person['email']}</p>`;
+          card += `</div>`;
+          card += `</div>`;
+
+          $('#peopleBox').append(card);
+
+
+
+        });
       },
       error: function(error){
         console.log(error);
